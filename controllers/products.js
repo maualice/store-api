166bd0c65aa419e3f9f6a57363e67bbeaf0802a9
maualice <alicemauroe@gmail.com>
2022-05-16 02:50:02 -0300

    corrigiendo bug que muestra vacio si paso una propiedad como query que no existe

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,23 @@
+const Product = require('../models/product');
+
 const getAllProductsStatic = async (req, res) => {
   //throw new Error('testing async errors');
-  res.status(200).json({ msg: 'products testing route' });
+  const products = await Product.find({
+    name: 'vase table',
+  });
+  res.status(200).json({ products, nbHits: products.length }); //msg: 'products testing route'
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'products route' });
+  const { featured } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false; // si featured no esta vacia,le asigno a el objeto creado objectQuery un campo llamado featured y le asigno true o false.Esto es para evitar bug que muestra vacio si paso una propiedad como query que no existe
+  }
+  console.log(req.query);
+  const products = await Product.find(queryObject); //await Product.find(req.query);//ojo con poner {name:req.query},primero que ya viene como objeto y segundo ya viene el campo a filtrar
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
