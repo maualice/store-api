970f01107f85db1b6c45b0785a1ee540800e0968
maualice <alicemauroe@gmail.com>
2022-05-18 18:35:18 -0300

    agregando la funcionalidad para ordenar el listado pasando el valor por query usando sort

--- controllers/products.js
+++ controllers/products.js
@@ -1,16 +1,12 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  //throw new Error('testing async errors');
-  const search = 'ab';
-  const products = await Product.find({
-    name: { $regex: search, $options: 'i' },
-  });
+  const products = await Product.find({}).sort('-name prince');
   res.status(200).json({ products, nbHits: products.length }); //msg: 'products testing route'
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query; //el usuario tiene tres maneras de filtrado
+  const { featured, company, name, sort } = req.query; //el usuario tiene tres maneras de filtrado
   const queryObject = {};
 
   if (featured) {
@@ -22,8 +18,15 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' }; //me permite buscar un string especifico dentro de una cadena,muy util si no se el valor exacto del campo
   }
-  console.log(req.query);
-  const products = await Product.find(queryObject); //await Product.find(req.query);//ojo con poner {name:req.query},primero que ya viene como objeto y segundo ya viene el campo a filtrar
+  //console.log(req.query);
+  let result = Product.find(queryObject); //Product.find(queryObject); //await Product.find(req.query);//ojo con poner {name:req.query},primero que ya viene como objeto y segundo ya viene el campo a filtrar
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createAt');
+  }
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
