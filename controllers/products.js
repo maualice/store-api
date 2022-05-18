87c204c36622cf9fe726c258ede94d3848ce681b
maualice <alicemauroe@gmail.com>
2022-05-18 20:41:16 -0300

    permitiendo seleccionar los campos que se quieren mostrar de la lista de productos usando select

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,12 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort('-name prince');
+  const products = await Product.find({}).select('name price');
   res.status(200).json({ products, nbHits: products.length }); //msg: 'products testing route'
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query; //el usuario tiene tres maneras de filtrado
+  const { featured, company, name, sort, fields } = req.query; //el usuario tiene tres maneras de filtrado //no necesariamente hay que poner el nombre exacto de la funcion,ej fields
   const queryObject = {};
 
   if (featured) {
@@ -20,13 +20,20 @@ const getAllProducts = async (req, res) => {
   }
   //console.log(req.query);
   let result = Product.find(queryObject); //Product.find(queryObject); //await Product.find(req.query);//ojo con poner {name:req.query},primero que ya viene como objeto y segundo ya viene el campo a filtrar
+  //sort
   if (sort) {
-    const sortList = sort.split(',').join(' ');
+    const sortList = sort.split(',').join(' '); // separa los valores que vienen con coma en sort y los vuelve a junar separandolos por espacio
     result = result.sort(sortList);
   } else {
-    result = result.sort('createAt');
+    result = result.sort('createAt'); // si el usuario no ingresa algo en sort,por default lo va a ordenar por createAt
   }
-  const products = await result;
+  //select
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
+  }
+
+  const products = await result; //el await se usa al final ya que si intento encadenarlo despues del await Product.find(queryObject) dara error,ya que me ya no tendria el product.find solo los resultados devueltos
   res.status(200).json({ products, nbHits: products.length });
 };
 
